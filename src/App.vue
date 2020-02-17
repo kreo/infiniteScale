6702ff4ef63ccfa2a95056a01cdceb0f82ab6c2d
Fabio Cencetti <dev@kreo.me>
2020-02-17 14:52:24 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <InfiniteScale/>
+  </div>
+</template>
+
+<script>
+import Skel from "./components/Skel";
+import InfiniteScale from "./components/InfiniteScale";
+
+export default {
+  name: "App",
+  components: {
+    Skel,
+    InfiniteScale
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#app {
+  padding: 1rem;
+}
+</style>
