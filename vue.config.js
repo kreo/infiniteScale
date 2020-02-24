d4ea98794163f2adb2414327e133ea9001aa1b42
Fabio Cencetti <dev@kreo.me>
2020-02-24 09:13:35 +0100

    chore: added magic importer

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,23 @@
+const path = require("path");
+
 module.exports = {
-    configureWebpack: {
-        devtool: 'source-map'
-    },
-   css: {
-      requireModuleExtension: false,
-      loaderOptions: {
-        sass: {
-          //prependData: `@import "~@/core/.sass"`
-        },
-      }
-   }
+  configureWebpack: {
+    devtool: 'source-map'
+  },
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "scss",
+      patterns: [
+        path.resolve(__dirname, "./src/styles/main.scss")
+      ]
+    }
+  },
+  css: {
+    requireModuleExtension: false,
+    loaderOptions: {
+      sass: {
+        //prependData: `@import "~@/core/.sass"`
+      },
+    }
+  }
 }
